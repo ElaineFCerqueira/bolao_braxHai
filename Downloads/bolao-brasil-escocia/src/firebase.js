9dc779f7e7fc862ba8fff92ae23972180f5a2911
Elaine Cerqueira <elaine@example.com>
2026-06-24 16:03:04 -0300

    fix: add Firebase config

--- Downloads/bolao-brasil-escocia/src/firebase.js
+++ Downloads/bolao-brasil-escocia/src/firebase.js
@@ -1,14 +1,22 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-}
+  apiKey: "AIzaSyANeiHzWNARvTSGFsWk7sGx8u-sgQbTXJc",
+  authDomain: "bolao-brasil-escocia-1356f.firebaseapp.com",
+  projectId: "bolao-brasil-escocia-1356f",
+  storageBucket: "bolao-brasil-escocia-1356f.firebasestorage.app",
+  messagingSenderId: "378577863551",
+  appId: "1:378577863551:web:1afad39ff6a87dd248d04a",
+  measurementId: "G-CGGTVZL339"
+};
 
-const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { app, analytics, db };
